36d47fbc03bba5812796e8ed069494d8ce5314d8
Sam-Hemphill-(SurfaceWin) <sam@meeum.com>
2018-07-29 13:19:03 +1000

    navigation and js

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -1,2 +1,19 @@
 // call cass vars function
 cssVars();
+
+
+// open and close the nav
+
+$('.nav-prompt').click(function() {
+
+    if ($('.nav-inner').attr('aria-expanded') == 'false') {
+
+        $('.nav-inner').attr('aria-expanded', true);
+
+    } else {
+
+        $('.nav-inner').attr('aria-expanded', false);
+    }
+
+
+});
